f963fe2dbe5767af61bb2b80fe33d0bd42bcc07a
Adnan Qureshi <72442243+Sparrow-13@users.noreply.github.com>
2021-05-22 18:06:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
-        
+var slider = document.getElementById("rate");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}     
