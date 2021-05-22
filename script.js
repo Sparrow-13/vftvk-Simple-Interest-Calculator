864ce494920d50ca0dac5e16d32b114167313e16
Adnan Qureshi <72442243+Sparrow-13@users.noreply.github.com>
2021-05-22 18:20:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    t = documnet.getElementById("years").value;
+    var si = (p*r*t)/100;
+    interest = document.getElementById("result").innerHTML="If you deposit : ";
+    interest.appendChild(p);
+    interest.innerHTML = "at an interest of : ";
+    interest.appendChild(r);
+    interest.innerHTML = "you will recieve  Amount of :  ";
+    interest.appendChild(si);
+    interest.innerHTML = "in year : ";
+    interest.appendChild(t);
     
 }
 var slider = document.getElementById("rate");
